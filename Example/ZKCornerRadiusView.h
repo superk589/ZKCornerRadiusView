0368f82b6c21f79571f672b8fc288a9dbe3b20ac
superk <superk589@gmail.com>
2017-09-08 08:58:24 +0800

    update for swift 4

---
+++ Example/ZKCornerRadiusView.h
@@ -0,0 +1,19 @@
+//
+//  ZKCornerRadiusView.h
+//  ZKCornerRadiusView
+//
+//  Created by zzk on 08/09/2017.
+//  Copyright © 2017 zzk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZKCornerRadiusView.
+FOUNDATION_EXPORT double ZKCornerRadiusViewVersionNumber;
+
+//! Project version string for ZKCornerRadiusView.
+FOUNDATION_EXPORT const unsigned char ZKCornerRadiusViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZKCornerRadiusView/PublicHeader.h>
+
+
